5ad990456b0f805fd2c2c2a0f4347824ac557110
chivsovanchitra <chivsovanchetra@gmail.com>
2025-07-22 17:59:10 +0700

    first commit

---
+++ .php-cs-fixer.php
@@ -0,0 +1,10 @@
+<?php
+
+return PhpCsFixer\Config::create()
+    ->setRules([
+        '@PSR2' => true,
+    ])
+    ->setFinder(
+        PhpCsFixer\Finder::create()
+            ->in(__DIR__)
+    );
